26c01e1f503edb5b1659e3d3eca63ef887863a01
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-05-10 07:41:12 +0530

    Cohort-51 W1-D3 Contest

---
+++ BASIC/print_masai_school.java
@@ -0,0 +1,30 @@
+package DSA.BASIC;
+
+import java.util.*;
+public class print_masai_school {
+    public static void main(String[] args) {
+        System.out.println("Masai School");
+        System.out.println("A Transformation in Education");
+    }
+}
+
+/*
+Print Masai School Ended
+Description
+
+You need to print "Masai School" on a line, and print "A Transformation in Education" on a new line.
+
+Input
+No input
+
+Output
+Print "Masai School" on a line, and Print "A Transformation in Education" on a new line.
+
+Sample Input 1
+
+No Input
+Sample Output 1
+
+Masai School
+A Transformation in Education
+ */
--- print_masai_school.java
+++
@@ -1,9 +0,0 @@
-package DSA;
-
-import java.util.*;
-public class print_masai_school {
-    public static void main(String[] args) {
-        System.out.println("Masai School");
-        System.out.println("A Transformation in Education");
-    }
-}
