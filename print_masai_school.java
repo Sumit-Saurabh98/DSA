c8df063901a4d8a9d8dbed5959ea346ee75613c9
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-05-10 07:03:45 +0530

    print_masai_School

---
+++ print_masai_school.java
@@ -0,0 +1,9 @@
+package DSA;
+
+import java.util.*;
+public class print_masai_school {
+    public static void main(String[] args) {
+        System.out.println("Masai School");
+        System.out.println("A Transformation in Education");
+    }
+}
